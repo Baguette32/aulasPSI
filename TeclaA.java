ce00d9b6c5b80b620e45013b271aef8efefddd12
Baguette32 <al.diogorocha1@aeaav.pt>
2019-04-23 13:06:11 +0100

    A vida é complicada

---
+++ TeclaA.java
@@ -0,0 +1,21 @@
+package teclaa;
+import java.util.Scanner;
+public class TeclaA {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int tempo=0;
+        tempo=sc.nextInt(); 
+        if (tempo>=5 && tempo<=7){
+           System.out.println("madrugada");
+        }
+        if (tempo>=8 && tempo<=12){
+           System.out.println("manha");
+        }
+        if (tempo>=13 && tempo<=19){
+           System.out.println("tarde");
+        }
+        if (tempo>=20 && tempo<=4){
+           System.out.println("noite");
+        }
+    }  
+}
